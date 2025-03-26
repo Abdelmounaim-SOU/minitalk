a3dca110368e2516199bf3ee0b93214797231ccd
Abdelmounaim Sou <asou@e2r12p4.1337.ma>
2025-03-26 23:36:50 +0000

    fixed norminette issues

--- src/client.c
+++ src/client.c
@@ -6,91 +6,93 @@
 /*   By: asou <asou@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 21:25:30 by asou              #+#    #+#             */
-/*   Updated: 2025/03/25 02:57:07 by asou             ###   ########.fr       */
+/*   Updated: 2025/03/26 23:34:54 by asou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    ft_error(char *str)
+#include "minitalk.h"
+
+void	ft_error(char *str)
 {
-    ft_printf("%s\n", str);
-    exit(1);
+	ft_printf("%s\n", str);
+	exit(1);
 }
 
-pid_t    ft_atoi_pid(const char *str)
+pid_t	ft_atoi_pid(const char *str)
 {
-    int pid;
+	int	pid;
 
-    pid = 0;
-    if (*str == '+')
-        str++;
-    while (*str)
-    {
-        if (*str < '0' || *str > '9')
-            return (-1);
+	pid = 0;
+	if (*str == '+')
+		str++;
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (-1);
 		if (pid >= PID_MAX)
-            return (-1);
-        pid = pid * 10 + *str - '0';
-        str++;
-    }
-    return ((pid_t)pid);
+			return (-1);
+		pid = pid * 10 + *str - '0';
+		str++;
+	}
+	return ((pid_t)pid);
 }
 
-void    send_char(pid_t pid, char c)
+void	send_char(pid_t pid, char c)
 {
-    int     i;
-    int     bit;
+	int	i;
+	int	bit;
 
-    i = 7;
-    while (i >= 0)
-    {ft_printf("pid est : %d\n",pid);
-        bit = (c >> i) & 1;
-        if (bit)
-        {
-            
-            if (kill(pid, SIGUSR1) == -1)
-                ft_error("Error: Signal not sent");            
-        }
-        else
-        {
-            if(kill(pid, SIGUSR2) == -1)
-                ft_error("Error: Signal not sent");
-        }
-        usleep(200);
-        i--;
-    }
+	i = 7;
+	while (i >= 0)
+	{
+		ft_printf("pid est : %d\n", pid);
+		bit = (c >> i) & 1;
+		if (bit)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				ft_error("Error: Signal not sent");
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				ft_error("Error: Signal not sent");
+		}
+		usleep(200);
+		i--;
+	}
 }
 
-void    send_str(pid_t pid, const char *str)
+void	send_str(pid_t pid, const char *str)
 {
-    while (*str)
-    {
-        send_char(pid, *str);
-        str++;
-    }
+	while (*str)
+	{
+		send_char(pid, *str);
+		str++;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    pid_t	pid;
+	pid_t	pid;
 
-    if (argc != 3)
-    {
-        ft_printf("Usage: %s <server-pid> <string>\n", argv[0]);
-        return (1);
-    }
-    pid = ft_atoi_pid(argv[1]);
-    if (pid == -1 || pid == 0)
-    {
-        ft_printf("Invalid PID: %s\n", argv[1]);
-        return (1);
-    }
-    if (kill(pid, 0) == -1)
-    {
-    	ft_printf("Server not found: %s\n", argv[1]);
-        return (1);
-    }
-    send_str(pid, argv[2]);
-    return (0);
+	if (argc != 3)
+	{
+		ft_printf("Usage: %s <server-pid> <string>\n", argv[0]);
+		return (1);
+	}
+	pid = ft_atoi_pid(argv[1]);
+	if (pid == -1 || pid == 0)
+	{
+		ft_printf("Invalid PID: %s\n", argv[1]);
+		return (1);
+	}
+	if (kill(pid, 0) == -1)
+	{
+		ft_printf("Server not found: %s\n", argv[1]);
+		return (1);
+	}
+	send_str(pid, argv[2]);
+	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: asou <asou@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 16:20:01 by asou              #+#    #+#             */
-/*   Updated: 2025/03/25 01:56:01 by asou             ###   ########.fr       */
+/*   Updated: 2025/03/26 23:32:25 by asou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,50 +18,50 @@ void	ft_error(char *str)
 	exit(1);
 }
 
-void signal_handler(int signum, siginfo_t *info, void *context)
+void	signal_handler(int signum, siginfo_t *info, void *context)
 {
-    static char c;
-    static int  i;
-    static pid_t pid;
+	static char		c;
+	static int		i;
+	static pid_t	pid;
 
-    (void)context;
-    if (pid != info->si_pid)
-    {
-        i = 0;
-        c = 0;
-    }
-    c = c | (signum == SIGUSR1);
-    i++;
-    if (i == 8)
-    {
-        write(1, &c, 1);
-        i = 0;
-        c = 0;
-    }
-    c<<=1;
-    pid = info->si_pid;
+	(void)context;
+	if (pid != info->si_pid)
+	{
+		i = 0;
+		c = 0;
+	}
+	c = c | (signum == SIGUSR1);
+	i++;
+	if (i == 8)
+	{
+		write(1, &c, 1);
+		i = 0;
+		c = 0;
+	}
+	c <<= 1;
+	pid = info->si_pid;
 }
 
-void    init_signal_handler(struct sigaction *sa)
+void	init_signal_handler(struct sigaction *sa)
 {
-    sa->sa_sigaction = signal_handler;
-    sa->sa_flags = SA_SIGINFO;
-    sigemptyset(&sa->sa_mask);
-    if (sigaction(SIGUSR1, sa, NULL) == -1)
+	sa->sa_sigaction = signal_handler;
+	sa->sa_flags = SA_SIGINFO;
+	sigemptyset(&sa->sa_mask);
+	if (sigaction(SIGUSR1, sa, NULL) == -1)
 		ft_error("Sigaction failed!\n");
-    if (sigaction(SIGUSR2, sa, NULL) == -1)
+	if (sigaction(SIGUSR2, sa, NULL) == -1)
 		ft_error("Sigaction failed!\n");
 }
 
-int main(void)
+int	main(void)
 {
-    pid_t   pid;
-    struct  sigaction sa;
-  
-    pid = getpid();
-    ft_printf("Server PID: %d\n", pid);
-    init_signal_handler(&sa);
-    while (1)
-        pause();
-    return (0);
+	pid_t				pid;
+	struct sigaction	sa;
+
+	pid = getpid();
+	ft_printf("Server PID: %d\n", pid);
+	init_signal_handler(&sa);
+	while (1)
+		pause();
+	return (0);
 }
